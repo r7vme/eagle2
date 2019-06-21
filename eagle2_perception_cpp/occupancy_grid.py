fd424bd9d5d8a3627c91f7f9e7b0299c9cb06457
Roma Sokolkov <roman@kopernikusauto.com>
2019-06-21 04:12:38 +0200

    (WIP) working mapping script

--- eagle2_perception_cpp/occupancy_grid.py
+++ eagle2_perception_cpp/occupancy_grid.py
@@ -19,26 +19,37 @@ def inv_logit(l):
   il = 1-(1/(1+np.exp(l)))
   return int(il*100)
 
-def occupancy_grid_mapping(grid_map, grid_update, orig_x, orig_y):
+def occupancy_grid_mapping(grid_map, grid_update, M):
   for i in range(grid_update.shape[0]):
     for j in range(grid_update.shape[1]):
-      p_cur=grid_map[orig_x+i][orig_y+j]
+      x = int(M[0][0]*i + M[0][1]*j + M[0][2])
+      y = int(M[1][0]*i + M[1][1]*j + M[1][2])
+
+      p_cur=grid_map[x][y]
       p_upd=grid_update[i][j]
       l_cur=logit(p_cur)
       l_upd=logit(p_upd)
       l_new=l_cur+l_upd
       p_new=inv_logit(l_new)
-      grid_map[orig_x+i][orig_y+j]=int(p_new)
+      grid_map[x][y]=int(p_new)
+
+# M = [cosQ, -sinQ, tx]
+#     [sinQ,  cosQ, ty]
+def get_affine_matrix(theta, tx, ty):
+  return np.array([[np.cos(theta), -np.sin(theta), tx],
+                   [np.sin(theta), np.cos(theta),  ty]])
+
+M = get_affine_matrix(0.1, 10, 20)
 
 # 20cm/px, 200meters x 200meters
 grid_map = np.full((1000,1000), 50, dtype=np.uint8)
-for i in range(10):
+for i in range(1):
   shift = i*10
   delta = 0
   # 20cm/px, 10meters x 10meters
   grid_update = np.full((50, 50), 53+delta, dtype=np.uint8)
   orig_x, orig_y = 100, 100+shift
-  occupancy_grid_mapping(grid_map, grid_update, orig_x, orig_y)
+  occupancy_grid_mapping(grid_map, grid_update, M)
   cv2.namedWindow("window")
   cv2.imshow("window", grid_map)
-  cv2.waitKey(100)
+  cv2.waitKey(0)
